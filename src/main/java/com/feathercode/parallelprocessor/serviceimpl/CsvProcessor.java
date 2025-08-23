74c23e24e6db1551962efa530deff34e4be91307
Doflamingo <varun4563232@gmail.com>
2025-08-23 14:05:06 +0530

    local commit

--- src/main/java/com/feathercode/parallelprocessor/serviceimpl/CsvProcessor.java
+++ src/main/java/com/feathercode/parallelprocessor/serviceimpl/CsvProcessor.java
@@ -2,34 +2,47 @@ package com.feathercode.parallelprocessor.serviceimpl;
 
 import com.feathercode.parallelprocessor.model.Product;
 import com.feathercode.parallelprocessor.service.DataProcessor;
+import com.feathercode.parallelprocessor.tasks.CsvBatchTask;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
 public class CsvProcessor implements DataProcessor {
+
+    private static final int BATCH_SIZE = 500;
     @Override
     public List<Product> readProducts(String path) {
+        ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
         List<Product> products = new ArrayList<>();
         try {
             BufferedReader bf = new BufferedReader(new FileReader(path));
             bf.readLine(); // to skip the header
+            List<String> batch = new ArrayList<>();
+            List<Future<List<Product>>> futures = new ArrayList<>();
             String line;
             while((line = bf.readLine()) != null) {
-                String[] parts = line.split(",");
-                Product p = new Product(
-                        parts[0],
-                        parts[1],
-                        parts[2],
-                        Integer.parseInt(parts[3]),
-                        Double.parseDouble(parts[4]));
-                products.add(p);
+                batch.add(line);
+                if(batch.size() == BATCH_SIZE) {
+                    futures.add(executor.submit(new CsvBatchTask(new ArrayList<>(batch))));
+                    batch.clear();
+                }
             }
+            if(!batch.isEmpty()) {
+                futures.add(executor.submit(new CsvBatchTask(new ArrayList<>(batch))));
+            }
+            for(Future<List<Product>> f : futures) products.addAll(f.get());
         } catch (Exception e){
-            System.out.println(e.getMessage());
+            System.out.println("Error while reading csv -> " + e.getMessage());
+        } finally {
+            executor.shutdown();
         }
         return products;
     }
